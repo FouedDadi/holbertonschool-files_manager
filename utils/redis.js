3b75f405f44894d5f19bb0a341c107d80eae814b
FouedDadi <foued.dadii@gmail.com>
2021-06-06 21:03:28 +0100

    adding utils directory

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import redis from 'redis';
+
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.get = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(K) {
+    return this.get(K, (err, response) => response);
+  }
+
+  async set(K, V, D) {
+    this.client.set(K, D, V);
+  }
+
+  async del(K) {
+    this.client.del(K);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
