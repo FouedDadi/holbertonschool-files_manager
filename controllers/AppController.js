b5d915b6bf385a1afc523955f6a18a763e3f4413
FouedDadi <foued.dadii@gmail.com>
2021-06-09 23:47:48 +0100

    adding files for routes and redis server

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,13 +1,13 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-export const getStatus = (req, res) => {
+export const getStatus = function sts(req, res) {
   const redis = redisClient.isAlive();
   const db = dbClient.isAlive();
   res.status(200).send({ redis, db });
 };
 
-export const getStats = async (req, res) => {
+export const getStats = async function stt(req, res) {
   const users = await dbClient.nbUsers();
   const files = await dbClient.nbFiles();
   res.status(200).send({ users, files });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
 import { getStatus, getStats } from '../controllers/AppController';
+
+const express = require('express');
 
 const router = express.Router();
 
