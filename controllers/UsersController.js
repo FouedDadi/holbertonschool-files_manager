45aec958a341542c4d73b3544fa979e2ecb41dba
FouedDadi <foued.dadii@gmail.com>
2021-06-10 22:55:07 +0100

    adding files to create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,28 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+const postNew = async function pstnw(request, result) {
+  const { email } = request.body;
+  const { password } = request.body;
+  if (!email) {
+    result.status(400).send(JSON.stringify('Missing email'));
+  }
+  if (!password) {
+    result.status(400).send(JSON.stringify('Missing password'));
+  }
+  const emailexst = await dbClient.client
+    .collection('users')
+    .findOne({ email });
+  if (emailexst) {
+    return result.status(400).send(JSON.stringify('Already exist'));
+  }
+  const hashedpasswrd = sha1(password);
+
+  const newusr = await dbClient.client
+    .collection('users')
+    .insertOne({ email, hashedpasswrd });
+  const generatedid = newusr.insertedId();
+  return result.status(201).json({ email, generatedid });
+};
+
+export default postNew;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import { getStatus, getStats } from '../controllers/AppController';
+import { postNew } from '../controllers/UsersController';
 
 const express = require('express');
 
@@ -6,5 +7,6 @@ const router = express.Router();
 
 router.get('/status', getStatus);
 router.get('/stats', getStats);
+router.post('/users', postNew);
 
 export default router;
